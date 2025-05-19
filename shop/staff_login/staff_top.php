0241ccd39dbbea8c21c17a9f2f8f6d3e7863a95f
chisato410 <it242243@trident.ac.jp>
2025-05-19 16:18:46 +0900

    ショップ管理メニューの編集

--- shop/staff_login/staff_top.php
+++ shop/staff_login/staff_top.php
@@ -11,21 +11,31 @@ if (isset($_SESSION['login']) == false) {
 <html>
 
 <head>
-  <meta charset="UTF-8">
-  <title>ショップ管理トップメニュー</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/the-new-css-reset/css/reset.min.css" />
+  <link rel="stylesheet" href="../css/style.min.css" />
+  <title>ショップ管理トップメニュー | ろくまる農園</title>
 </head>
 
+
 <body>
-  ショップ管理トップメニュー
-  <br>
-  <br>
-  <a href="../staff/staff_list.php">スタッフ管理</a>
-  <br>
-  <a href="../product/pro_list.php">商品管理</a>
-  <br>
-  <a href="../order/order_download.php">注文ダウンロード</a>
-  <br>
-  <a href="staff_logout.php">ログアウトする</a>
+  <?php include '../common/header.php'; ?>
+
+  <main class="main">
+    <div class="main__inner">
+      <h2 class="main__heading">ショップ管理トップメニュー</h2>
+
+      <div class="main__btns">
+        <a href="../staff/staff_list.php" class="main__btn">スタッフ管理</a>
+        <a href="../product/pro_list.php" class="main__btn">商品管理</a>
+        <a href="../order/order_download.php" class="main__btn">注文ダウンロード</a>
+        <a href="staff_logout.php" class="main__btn">ログアウトする</a>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
