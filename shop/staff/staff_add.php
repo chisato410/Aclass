c74f176aa22a81766e1fdd5550b002a411c0fb9d
chisato410 <it242243@trident.ac.jp>
2025-05-26 14:54:54 +0900

    staff_addにcssを追加

--- shop/staff/staff_add.php
+++ shop/staff/staff_add.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+
+if (!isset($_SESSION['login'])) {
+  echo 'ログインしていません<br>';
+  echo '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
-} else {
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中 <br> ';
-  print '<br>';
 }
 ?>
 
@@ -17,25 +14,42 @@ if (isset($_SESSION['login']) == false) {
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
+  <title>スタッフ追加 | ろくまる農園</title>
 </head>
 
 <body>
-  スタッフ追加 <br>
-  <br>
-  <form method="post" action="staff_add_check.php">
-    スタッフ名を入力してください。<br>
-    <input type="text" name="name" style="width: 200px;"><br>
-    パスワードを入力してください。<br>
-    <input type="password" name="pass" style="width: 100px;"><br>
-    パスワードをもう１度入力してください。<br>
-    <input type="password" name="pass2" style="width: 100px;"><br>
-    <br>
-    <input type="button" onclick="history.back()" value="戻る">
-    <input type="submit" value="OK">
-  </form>
+  <?php include '../common/header.php'; ?>
+
+  <main class="main">
+    <div class="main__inner">
+      <h2 class="form__title">スタッフ追加</h2>
+
+      <form method="post" action="staff_add_check.php" class="form">
+
+        <div class="form-group">
+          <label for="name">スタッフ名を入力してください。</label><br>
+          <input type="text" name="name" id="name" class="form__input" style="width: 200px;">
+        </div>
+
+        <div class="form-group">
+          <label for="pass">パスワードを入力してください。</label><br>
+          <input type="password" name="pass" id="pass" class="form__input" style="width: 100px;">
+        </div>
+
+        <div class="form-group">
+          <label for="pass2">パスワードをもう１度入力してください。</label><br>
+          <input type="password" name="pass2" id="pass2" class="form__input" style="width: 100px;">
+        </div>
+
+        <div class="form-actions">
+          <input type="button" onclick="history.back()" value="戻る" class="link-back">
+          <input type="submit" value="OK" class="main__submit">
+        </div>
+
+      </form>
+    </div>
+  </main>
 </body>
 
 </html>
