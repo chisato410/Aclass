912dc8d0db2d97fbf4855330e8abb047fb07dff3
chisato410 <it242243@trident.ac.jp>
2025-05-26 15:02:45 +0900

    staff_add_doneにcssを追加

--- shop/staff/staff_add_done.php
+++ shop/staff/staff_add_done.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+
+if (!isset($_SESSION['login'])) {
+  echo 'ログインしていません<br>';
+  echo '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
-} else {
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中 <br> ';
-  print '<br>';
 }
 ?>
 
@@ -17,45 +14,53 @@ if (isset($_SESSION['login']) == false) {
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
+  <title>スタッフ追加実行 | ろくまる農園</title>
 </head>
 
 <body>
-  <?php
-  try {
-    require_once('../common/common.php');
-    $post = sanitize($_POST);
+  <?php include '../common/header.php'; ?>
 
-    $staff_name = $post['name'];
-    $staff_pass = $post['pass'];
+  <main class="main">
+    <div class="main__inner">
+      <?php
+      try {
+        require_once('../common/common.php');
+        $post = sanitize($_POST);
+        $staff_name = $post['name'];
+        $staff_pass = $post['pass'];
 
-    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
-    $user = 'root';
-    $password = 'root';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'INSERT INTO mst_staff(name,password) VALUES (?,?)';
-    $stmt = $dbh->prepare($sql);
-    $data[] = $staff_name;
-    $data[] = $staff_pass;
-    $stmt->execute($data);
+        $sql = 'INSERT INTO mst_staff(name,password) VALUES (?,?)';
+        $stmt = $dbh->prepare($sql);
+        $data[] = $staff_name;
+        $data[] = $staff_pass;
+        $stmt->execute($data);
 
-    $dbh = null;
+        $dbh = null;
 
-    print $staff_name;
-    print 'さんを追加しました。<br>';
-  } catch (Exception $e) {
-    print 'ただいま障害により大変ご迷惑をお掛けしております。';
-    print $e->getMessage();
-    exit();
-  }
+        echo '<div class="form__result">';
+        echo '<p class="result">' . htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8') . ' さんを追加しました。</p>';
+        echo '</div>';
+      } catch (Exception $e) {
+        echo '<div class="form__error">';
+        echo '<p>ただいま障害により大変ご迷惑をお掛けしております。</p>';
+        echo '<p>' . $e->getMessage() . '</p>';
+        echo '</div>';
+        exit();
+      }
+      ?>
 
-  ?>
-
-  <a href="staff_list.php">戻る</a>
+      <div class="form-actions">
+        <a href="staff_list.php" class="link-back">戻る</a>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
