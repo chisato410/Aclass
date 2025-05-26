22dcbc015276c87a83cd8c266bc3a2d2c4971ad7
chisato410 <it242243@trident.ac.jp>
2025-05-26 16:35:02 +0900

    商品の削除の修正と画像ファイルの統一

--- shop/product/pro_delete.php
+++ shop/product/pro_delete.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+
+if (!isset($_SESSION['login'])) {
+  echo 'ログインしていません<br>';
+  echo '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
-} else {
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中 <br> ';
-  print '<br>';
 }
 ?>
 
@@ -16,64 +13,68 @@ if (isset($_SESSION['login']) == false) {
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
+  <title>商品削除 | ろくまる農園</title>
 </head>
 
 <body>
-  <?php
-  try {
-    $pro_code = $_GET['procode'];
+  <?php include '../common/header.php'; ?>
 
-    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
-    $user = 'root';
-    $password = 'root';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  <main class="main">
+    <div class="main__inner">
+      <?php
+      try {
+        $pro_code = $_GET['procode'];
 
-    $sql = 'SELECT name,gazou FROM mst_product WHERE code=?';
-    $stmt = $dbh->prepare($sql);
-    $data[] = $pro_code;
-    $stmt->execute($data);
+        $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-    $pro_name = $rec['name'];
-    $pro_gazou_name = $rec['gazou'];
+        $sql = 'SELECT name, gazou FROM mst_product WHERE code=?';
+        $stmt = $dbh->prepare($sql);
+        $data[] = $pro_code;
+        $stmt->execute($data);
 
-    $dbh = null;
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+        $pro_name = $rec['name'];
+        $pro_gazou_name = $rec['gazou'];
 
-    if ($pro_gazou_name == '') {
-      $disp_gazou = '';
-    } else {
-      $disp_gazou = '<img src ="./gazou/' . $pro_gazou_name . '">';
-    }
-  } catch (Exception $e) {
-    print 'ただいま障害により大変ご迷惑をおかけしております。';
-    print $e->getMessage();
-    exit();
-  }
+        $dbh = null;
 
-  ?>
+        $disp_gazou = ($pro_gazou_name != '') ? '<img src="./gazou/' . htmlspecialchars($pro_gazou_name, ENT_QUOTES, 'UTF-8') . '">' : '';
+      } catch (Exception $e) {
+        echo '<div class="form__error">';
+        echo '<p>ただいま障害により大変ご迷惑をおかけしております。</p>';
+        echo '<p>' . $e->getMessage() . '</p>';
+        echo '</div>';
+        exit();
+      }
+      ?>
 
-  商品削除<br>
-  <br>
-  商品コード<br>
-  <?php print $pro_code; ?>
-  <br>
-  商品名<br>
-  <?php print $pro_name; ?>
-  <br>
-  <?php print $disp_gazou; ?>
-  <br>
-  この商品を削除してよろしいですか？<br>
-  <br>
-  <form action="pro_delete_done.php" method="post">
-    <input type="hidden" name="code" value="<?php print $pro_code; ?>">
-    <input type="hidden" name="gazou_name" value="<?php print $pro_gazou_name; ?>">
-    <input type="button" value="戻る" onclick="history.back()">
-    <input type="submit" value="OK">
-  </form>
+      <div class="form__container">
+        <h2 class="form__heading">商品削除</h2>
+
+        <div class="form__info">
+          <p><strong>商品コード：</strong><?php echo htmlspecialchars($pro_code, ENT_QUOTES, 'UTF-8'); ?></p>
+          <p><strong>商品名：</strong><?php echo htmlspecialchars($pro_name, ENT_QUOTES, 'UTF-8'); ?></p>
+          <?php if ($disp_gazou): ?>
+            <div class="form__image"><?php echo $disp_gazou; ?></div>
+          <?php endif; ?>
+        </div>
+
+        <p class="form__confirm">この商品を削除してよろしいですか？</p>
+
+        <form action="pro_delete_done.php" method="post" class="form-actions">
+          <input type="hidden" name="code" value="<?php echo htmlspecialchars($pro_code, ENT_QUOTES, 'UTF-8'); ?>">
+          <input type="hidden" name="gazou_name" value="<?php echo htmlspecialchars($pro_gazou_name, ENT_QUOTES, 'UTF-8'); ?>">
+          <input type="button" class="link-back" value="戻る" onclick="history.back()">
+          <input type="submit" class="main__submit" value="OK">
+        </form>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
--- shop/product/pro_delete_done.php
+++ shop/product/pro_delete_done.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+
+if (!isset($_SESSION['login'])) {
+  echo 'ログインしていません<br>';
+  echo '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
-} else {
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中 <br> ';
-  print '<br>';
 }
 ?>
 
@@ -16,44 +13,50 @@ if (isset($_SESSION['login']) == false) {
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
+  <title>商品削除実行 | ろくまる農園</title>
 </head>
 
 <body>
-  <?php
-  try {
+  <?php include '../common/header.php'; ?>
 
-    $pro_code = $_POST['code'];
-    $pro_gazou_name = $_POST['gazou_name'];
+  <main class="main">
+    <div class="main__inner">
+      <?php
+      try {
+        $pro_code = $_POST['code'];
+        $pro_gazou_name = $_POST['gazou_name'];
 
-    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
-    $user = 'root';
-    $password = 'root';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'DELETE FROM mst_product WHERE code=?';
-    $stmt = $dbh->prepare($sql);
-    $data[] = $pro_code;
-    $stmt->execute($data);
+        $sql = 'DELETE FROM mst_product WHERE code=?';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute([$pro_code]);
 
-    $dbh = null;
+        $dbh = null;
 
-    if ($pro_gazou_name != '') {
-      unlink('./gazou/' . $pro_gazou_name);
-    }
-  } catch (Exception $e) {
-    print 'ただいま障害により大変ご迷惑をお掛けしております。';
-    print $e->getMessage();
-    exit();
-  }
+        if (!empty($pro_gazou_name)) {
+          unlink('./gazou/' . $pro_gazou_name);
+        }
+      } catch (Exception $e) {
+        echo '<div class="form__error">';
+        echo '<p>ただいま障害により大変ご迷惑をお掛けしております。</p>';
+        echo '<p>' . $e->getMessage() . '</p>';
+        echo '</div>';
+        exit();
+      }
+      ?>
 
-  ?>
-  削除しました。<br>
-  <br>
-  <a href="pro_list.php">戻る</a>
+      <div class="form-container">
+        <p class="form__message">削除しました。</p>
+        <a class="link-back" href="pro_list.php">戻る</a>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
