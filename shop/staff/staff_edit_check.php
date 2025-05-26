24f14943f9277aee71b10bd6b8ff4b5f7e03e25d
chisato410 <it242243@trident.ac.jp>
2025-05-26 14:43:16 +0900

    staff_edit_checkにcssの追加

--- shop/staff/staff_edit_check.php
+++ shop/staff/staff_edit_check.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+
+if (!isset($_SESSION['login'])) {
+  echo 'ログインしていません<br>';
+  echo '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
-} else {
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中 <br> ';
-  print '<br>';
 }
 ?>
 
@@ -17,54 +14,69 @@ if (isset($_SESSION['login']) == false) {
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
+  <title>スタッフ修正 | ろくまる農園</title>
 </head>
 
 <body>
-  <?php
-  require_once('../common/common.php');
-  $post = sanitize($_POST);
+  <?php include '../common/header.php'; ?>
 
-  $staff_code = $post['code'];
-  $staff_name = $post['name'];
-  $staff_pass = $post['pass'];
-  $staff_pass2 = $post['pass2'];
+  <main class="main">
+    <main class="main__inner">
+      <?php
+      require_once('../common/common.php');
 
+      $post = sanitize($_POST);
+      $staff_code = $post['code'];
+      $staff_name = $post['name'];
+      $staff_pass = $post['pass'];
+      $staff_pass2 = $post['pass2'];
 
-  if ($staff_name == '') {
-    print 'スタッフ名が入力されていません。<br>';
-  } else {
-    print 'スタッフ名：';
-    print $staff_name;
-    print '<br>';
-  }
+      $has_error = false;
 
-  if ($staff_pass == '') {
-    print 'パスワードが入力されていません。<br>';
-  }
+      echo '<div class="form-result">';
 
-  if ($staff_pass != $staff_pass2) {
-    print 'パスワードが一致しません。<br>';
-  }
+      if ($staff_name == '') {
+        echo '<p class="error">スタッフ名が入力されていません。</p>';
+        $has_error = true;
+      } else {
+        echo '<p>スタッフ名：' . htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8') . '</p>';
+      }
 
-  if ($staff_name == '' || $staff_pass == '' || $staff_pass != $staff_pass2) {
-    print '<form>';
-    print '<input type="button" onclick="history.back()" value="戻る">';
-    print '</form>';
-  } else {
-    $staff_pass = md5($staff_pass);
-    print '<form method="post" action="staff_edit_done.php">';
-    print '<input type="hidden" name="code" value="' . $staff_code . '">';
-    print '<input type="hidden" name="name" value="' . $staff_name . '">';
-    print '<input type="hidden" name="pass" value="' . $staff_pass . '">';
-    print '<br>';
-    print '<input type="button" onclick="history.back()" value="戻る">';
-    print '<input type="submit" value="OK">';
-    print '</form>';
-  }
-  ?>
+      if ($staff_pass == '') {
+        echo '<p class="error">パスワードが入力されていません。</p>';
+        $has_error = true;
+      }
+
+      if ($staff_pass != $staff_pass2) {
+        echo '<p class="error">パスワードが一致しません。</p>';
+        $has_error = true;
+      }
+
+      if ($has_error) {
+        echo '<div class="form-actions">';
+        echo '<form>';
+        echo '<input type="button" onclick="history.back()" value="戻る" class="link-back">';
+        echo '</form>';
+        echo '</div>';
+      } else {
+        $staff_pass = md5($staff_pass);
+
+        echo '<form method="post" action="staff_edit_done.php">';
+        echo '<input type="hidden" name="code" value="' . htmlspecialchars($staff_code, ENT_QUOTES, 'UTF-8') . '">';
+        echo '<input type="hidden" name="name" value="' . htmlspecialchars($staff_name, ENT_QUOTES, 'UTF-8') . '">';
+        echo '<input type="hidden" name="pass" value="' . $staff_pass . '">';
+
+        echo '<div class="form-actions">';
+        echo '<input type="button" onclick="history.back()" value="戻る" class="link-back">';
+        echo '<input type="submit" value="OK" class="main__submit">';
+        echo '</div>';
+        echo '</form>';
+      }
+
+      echo '</div>';
+      ?> </main>
+  </main>
 </body>
 
 </html>
