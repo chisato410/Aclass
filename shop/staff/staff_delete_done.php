8d6c3bdc2dca1a5f6c7adc2ac984cfe413bd0448
chisato410 <it242243@trident.ac.jp>
2025-05-26 15:11:45 +0900

    staff_delete_doneにcssの追加

--- shop/staff/staff_delete_done.php
+++ shop/staff/staff_delete_done.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+
+if (!isset($_SESSION['login'])) {
+  echo 'ログインしていません<br>';
+  echo '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
-} else {
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中 <br> ';
-  print '<br>';
 }
 ?>
 
@@ -17,39 +14,46 @@ if (isset($_SESSION['login']) == false) {
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
+  <title>スタッフ削除実行 | ろくまる農園</title>
 </head>
 
 <body>
-  <?php
-  try {
+  <?php include '../common/header.php'; ?>
 
-    $staff_code = $_POST['code'];
+  <main class="main">
+    <div class="main__inner">
+      <?php
+      try {
+        $staff_code = $_POST['code'];
 
-    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
-    $user = 'root';
-    $password = 'root';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'DELETE FROM mst_staff WHERE code=?';
-    $stmt = $dbh->prepare($sql);
-    $data[] = $staff_code;
-    $stmt->execute($data);
+        $sql = 'DELETE FROM mst_staff WHERE code=?';
+        $stmt = $dbh->prepare($sql);
+        $data[] = $staff_code;
+        $stmt->execute($data);
 
-    $dbh = null;
-  } catch (Exception $e) {
-    print 'ただいま障害により大変ご迷惑をお掛けしております。';
-    print $e->getMessage();
-    exit();
-  }
+        $dbh = null;
+      } catch (Exception $e) {
+        echo '<div class="form__error">';
+        echo '<p>ただいま障害により大変ご迷惑をお掛けしております。</p>';
+        echo '<p>' . $e->getMessage() . '</p>';
+        echo '</div>';
+        exit();
+      }
+      ?>
 
-  ?>
-  削除しました。<br>
-  <br>
-  <a href="staff_list.php">戻る</a>
+      <div class="form-container">
+        <p class="form__message">削除しました。</p>
+        <a class="link-back" href="staff_list.php">戻る</a>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
