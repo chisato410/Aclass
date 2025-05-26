c6018e855b07ad910784d8e39f230723510ff104
chisato410 <it242243@trident.ac.jp>
2025-05-26 16:05:35 +0900

    pro_ngにcssを追加

--- shop/product/pro_ng.php
+++ shop/product/pro_ng.php
@@ -1,14 +1,11 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+
+if (!isset($_SESSION['login'])) {
+  echo 'ログインしていません<br>';
+  echo '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
-} else {
-  print $_SESSION['staff_name'];
-  print 'さんがログイン中 <br> ';
-  print '<br>';
 }
 ?>
 
@@ -16,14 +13,22 @@ if (isset($_SESSION['login']) == false) {
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
+  <title>未選択エラー | ろくまる農園</title>
 </head>
 
 <body>
-  商品が選択されていません。<br>
-  <a href="./pro_list.php">戻る</a>
+  <?php include '../common/header.php'; ?>
+
+  <main class="main">
+    <div class="main__inner">
+      <p class="result">商品が選択されていません。</p>
+      <div class="form-actions">
+        <a href="./pro_list.php" class="link-back">戻る</a>
+      </div>
+    </div>
+  </main>
+
 </body>
 
 </html>
