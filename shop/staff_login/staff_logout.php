db555f5b8cef806a3cc81338a7ca1a06c1c3032c
chisato410 <it242243@trident.ac.jp>
2025-06-02 14:55:17 +0900

    staff_logoutにcssを追加

--- shop/staff_login/staff_logout.php
+++ shop/staff_login/staff_logout.php
@@ -1,23 +1,34 @@
 <?php
 session_start();
+
+// セッション変数を空に
 $_SESSION = [];
-if (isset($_COOKIE[session_name()]) == true) {
+
+// セッションIDに関連付けられたクッキーの削除
+if (isset($_COOKIE[session_name()])) {
   setcookie(session_name(), '', time() - 42000, '/');
-  session_destroy();
 }
+
+// セッションの破棄
+session_destroy();
 ?>
-<!doctype html>
-<html>
 
 <head>
-  <meta charset="UTF-8">
-  <title>ログアウト</title>
+  <?php include '../common/head.php'; ?>
+  <title>ログアウト | ろくまる農園</title>
 </head>
 
 <body>
-  ログアウトしました。
-  <br>
-  <a href="../staff_login/staff_login.html">ログイン画面へ</a>
+  <?php include '../common/header.php'; ?>
+
+
+  <main class="main">
+    <main class="main__inner">
+      <p class="result">ログアウトしました。</p>
+      <p><a href="../staff_login/staff_login.html" class="link-back">ログイン画面へ</a></p>
+    </main>
+  </main>
+
 </body>
 
 </html>
