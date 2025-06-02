a915f344faaf05b82a7e3612e2b798909e837323
chisato410 <it242243@trident.ac.jp>
2025-06-02 15:06:21 +0900

    topにスタッフ名の追加

--- shop/staff_login/staff_top.php
+++ shop/staff_login/staff_top.php
@@ -1,20 +1,19 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
+if (empty($_SESSION['login'])) {
   print 'ログインしていません<br>';
   print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
 }
 ?>
 <!doctype html>
-<html>
+<html lang="ja">
 
 <head>
   <?php include '../common/head.php'; ?>
   <title>ショップ管理トップメニュー | ろくまる農園</title>
 </head>
-
 
 <body>
   <?php include '../common/header.php'; ?>
@@ -23,11 +22,19 @@ if (isset($_SESSION['login']) == false) {
     <div class="main__inner">
       <h2 class="main__heading">ショップ管理トップメニュー</h2>
 
+      <p class="main__login-user">
+        ようこそ、<?php echo htmlspecialchars($_SESSION['staff_name'], ENT_QUOTES, 'UTF-8'); ?>さん
+      </p>
+
       <div class="main__btns">
         <a href="../staff/staff_list.php" class="main__btn">スタッフ管理</a>
         <a href="../product/pro_list.php" class="main__btn">商品管理</a>
         <a href="../order/order_download.php" class="main__btn">注文ダウンロード</a>
-        <a href="staff_logout.php" class="main__btn">ログアウトする</a>
+
+        <!-- POSTでのログアウト推奨 -->
+        <form action="staff_logout.php" method="post" style="display:inline;">
+          <button type="submit" class="main__btn">ログアウトする</button>
+        </form>
       </div>
     </div>
   </main>
