86739419313f9daabffabb687be06c58778ac914
chisato410 <it242243@trident.ac.jp>
2025-06-02 16:10:38 +0900

    アイコンフォントの追加

--- shop/common/head.php
+++ shop/common/head.php
@@ -4,3 +4,4 @@
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/the-new-css-reset/css/reset.min.css" />
 <link rel="stylesheet" href="../css/style.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
