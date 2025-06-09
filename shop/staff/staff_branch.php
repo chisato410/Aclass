65cfadae9a770e283113b138bd31f7240b7efe61
chisato410 <it242243@trident.ac.jp>
2025-06-09 15:40:49 +0900

    スタッフブランチのコードを整理し、条件文を簡素化して可読性を向上

--- shop/staff/staff_branch.php
+++ shop/staff/staff_branch.php
@@ -1,48 +1,47 @@
 <?php
-
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['login']) == false) {
-  print 'ログインされていません。<br>';
-  print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+
+if (empty($_SESSION['login'])) {
+  echo 'ログインされていません。<br>';
+  echo '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
   exit();
 }
 
-if (isset($_POST['disp']) == true) {
-  if (isset($_POST['staffcode']) == false) {
-    header('Location:staff_ng.php');
+$staff_code = $_POST['staffcode'] ?? null;
+
+// 表示ボタンが押されたとき
+if (isset($_POST['disp'])) {
+  if ($staff_code === null) {
+    header('Location: staff_ng.php');
     exit();
   }
-  $staff_code = $_POST['staffcode'];
-  header('Location: staff_edit.php?staffcode=' . $staff_code);
+  header('Location: staff_disp.php?staffcode=' . rawurlencode($staff_code));
   exit();
 }
 
-
-if (isset($_POST['add']) == true) {
-  header('Location:staff_add.php');
+// 追加ボタンが押されたとき
+if (isset($_POST['add'])) {
+  header('Location: staff_add.php');
   exit();
 }
 
-if (isset($_POST['edit']) == true) {
-  if (isset($_POST['staffcode']) == false) {
-    header('Location:staff_ng.php');
+// 修正ボタンが押されたとき
+if (isset($_POST['edit'])) {
+  if ($staff_code === null) {
+    header('Location: staff_ng.php');
     exit();
   }
-
-  // print '修正ボタンが押された';
-  $staff_code = $_POST['staffcode'];
-  header('Location: staff_edit.php?staffcode=' . $staff_code);
+  header('Location: staff_edit.php?staffcode=' . rawurlencode($staff_code));
+  exit();
 }
 
-if (isset($_POST['delete']) == true) {
-  if (isset($_POST['staffcode']) == false) {
-    header('Location:staff_ng.php');
+// 削除ボタンが押されたとき
+if (isset($_POST['delete'])) {
+  if ($staff_code === null) {
+    header('Location: staff_ng.php');
     exit();
   }
-
-  // print '削除ボタンが押された';
-  $staff_code = $_POST['staffcode'];
-  header('Location: staff_delete.php?staffcode=' . $staff_code);
+  header('Location: staff_delete.php?staffcode=' . rawurlencode($staff_code));
   exit();
 }
