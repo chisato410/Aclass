f1c32a88c4bcf3f7b5f229edab5e87ced63a0084
chisato410 <it242243@trident.ac.jp>
2025-06-23 14:52:44 +0900

    商品ページにcssの追加

--- shop/shop/shop_list.php
+++ shop/shop/shop_list.php
@@ -1,67 +1,67 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['member_login']) == false) {
-  $member_message = 'ようこそ、ゲスト様 ';
-  $member_message .= '<a href="member_login.html">会員ログイン</a>';
-  $member_message .= '<br><br>';
+
+if (!isset($_SESSION['member_login'])) {
+  $member_message = <<<HTML
+ようこそ、ゲスト様
+<a href="member_login.html" class="login__btn">会員ログイン</a><br><br>
+HTML;
 } else {
-  $member_message = 'ようこそ、';
-  $member_message .= $_SESSION['member_name'];
-  $member_message .= '様 ';
-  $member_message .= '<a href="member_logout.php">ログアウト</a>';
-  $member_message .= '<br><br>';
+  $name = htmlspecialchars($_SESSION['member_name'], ENT_QUOTES, 'UTF-8');
+  $member_message = <<<HTML
+ようこそ、{$name}様
+<a href="member_logout.php" class="login__btn">ログアウト</a><br><br>
+HTML;
 }
 ?>
+
 <!doctype html>
 <html>
 
 <head>
-  <meta charset="UTF-8">
-  <title> ろくまる農園 商品一覧</title>
+  <?php include '../common/head.php'; ?>
+  <title>商品一覧 | ろくまる農園</title>
 </head>
 
 <body>
+  <?php include '../common/header.php'; ?>
 
-  <?php
+  <main class="main">
+    <div class="main__inner">
+      <?php
+      print $member_message;
+      try {
+        // データベースへ接続する
+        $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8mb4';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // データを表示する
+        $sql = 'SELECT code,name,price FROM mst_product WHERE 1';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $dbh = null;
+        print '商品一覧<br><br>';
 
-  print $member_message;
-
-  try {
-
-    // データベースへ接続する
-    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8mb4';
-    $user = 'root';
-    $password = 'root';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // データを表示する
-    $sql = 'SELECT code,name,price FROM mst_product WHERE 1';
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
-    $dbh = null;
-
-    print '商品一覧<br><br>';
-
-    while ($rec = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      print '<a href="shop_product.php?procode=' . $rec['code'] . '">';
-      print $rec['name'];
-      print '---';
-      print $rec['price'];
-      print '円';
-      print '</a>';
-      print '<br>';
-    }
-  } catch (Exception $e) {
-    print 'ただいま障害により大変ご迷惑をおかけしております。';
-    print $e->getMessage(); // エラーメッセージを出す
-    exit();
-  }
-  ?>
-
-  <br>
-  <a href="shop_cartlook.php">カートを見る</a>
+        print '<div class="product-list">';
+        while ($rec = $stmt->fetch(PDO::FETCH_ASSOC)) {
+          print '<a href="shop_product.php?procode=' . $rec['code'] . '" class="product-list__item">';
+          print '<span>' . htmlspecialchars($rec['name'], ENT_QUOTES, 'UTF-8') . '</span>';
+          print '--- ';
+          print htmlspecialchars($rec['price'], ENT_QUOTES, 'UTF-8') . '円';
+          print '</a>';
+        }
+        print '</div>';
+        print '<a href="shop_cartlook.php" class="cart-link">カートを見る</a>';
+      } catch (Exception $e) {
+        print 'ただいま障害により大変ご迷惑をお掛けしております。';
+        exit();
+      }
+      ?>
+    </div>
+  </main>
 </body>
 
 </html>
