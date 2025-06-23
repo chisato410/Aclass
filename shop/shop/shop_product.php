eac25debb505e3687afd2841fdeb568d24e598f6
chisato410 <it242243@trident.ac.jp>
2025-06-23 15:43:45 +0900

    商品個別ページにcssを追加

--- shop/shop/shop_product.php
+++ shop/shop/shop_product.php
@@ -1,93 +1,99 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['member_login']) == false) {
-  $member_message = 'ようこそ、ゲスト様 ';
-  $member_message .= '<a href="member_login.html">会員ログイン</a>';
-  $member_message .= '<br><br>';
+
+if (!isset($_SESSION['member_login'])) {
+  $member_message = <<<HTML
+ようこそ、ゲスト様
+<a href="member_login.html" class="login__btn">会員ログイン</a><br><br>
+HTML;
 } else {
-  $member_message = 'ようこそ、';
-  $member_message .= $_SESSION['member_name'];
-  $member_message .= '様 ';
-  $member_message .= '<a href="member_logout.php">ログアウト</a>';
-  $member_message .= '<br><br>';
+  $name = htmlspecialchars($_SESSION['member_name'], ENT_QUOTES, 'UTF-8');
+  $member_message = <<<HTML
+ようこそ、{$name}様
+<a href="member_logout.php" class="login__btn">ログアウト</a><br><br>
+HTML;
 }
 ?>
 <!doctype html>
-<html>
+<html lang="ja">
 
 <head>
   <meta charset="UTF-8">
-  <title> ろくまる農園 商品情報参照</title>
+  <title>商品情報参照 | ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
 </head>
 
 <body>
+  <?php include '../common/header.php'; ?>
 
-  <?php
+  <main class="main">
+    <div class="main__inner">
+      <?php echo $member_message; ?>
 
-  print $member_message;
+      <?php
+      try {
+        $pro_code = filter_input(INPUT_GET, 'procode', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
+        $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8mb4';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  try {
-    $pro_code = filter_input(INPUT_GET, 'procode', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $sql = 'SELECT name, price, gazou FROM mst_product WHERE code = ?';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute([$pro_code]);
 
-    // データベースへ接続する
-    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8mb4';
-    $user = 'root';
-    $password = 'root';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!$rec) {
+          echo '<p>商品情報が見つかりません。</p>';
+          exit();
+        }
 
-    // データを表示する
-    $sql = 'SELECT name,price,gazou FROM mst_product WHERE code=?';
-    $stmt = $dbh->prepare($sql);
-    $data = [$pro_code];
-    $stmt->execute($data);
+        $pro_name = htmlspecialchars($rec['name'], ENT_QUOTES, 'UTF-8');
+        $pro_price = htmlspecialchars($rec['price'], ENT_QUOTES, 'UTF-8');
+        $pro_gazou_name = $rec['gazou'];
 
-    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-    $pro_name = $rec['name'];
-    $pro_price = $rec['price'];
-    $pro_gazou_name = $rec['gazou'];
-    $dbh = null;
+        $disp_gazou = '';
+        if ($pro_gazou_name) {
+          $gazou_file = '../product/gazou/' . htmlspecialchars($pro_gazou_name, ENT_QUOTES, 'UTF-8');
+          $disp_gazou = '<img src="' . $gazou_file . '" alt="" style="max-width:200px; height:auto;">';
+        }
 
-    $disp_gazou = '';
-    if ($pro_gazou_name) {
-      $gazou_file = '../product/gazou/' . $pro_gazou_name;
-      $disp_gazou = '<img src="' . $gazou_file . '">';
-    }
-    print '<a href="shop_cartin.php?procode=' . $pro_code . '">カートに入れる</a>';
-    print '<br><br>';
-  } catch (Exception $e) {
-    print 'ただいま障害により大変ご迷惑をおかけしております。';
-    print $e->getMessage(); // エラーメッセージを出す
-    exit();
-  }
-  ?>
+        $dbh = null;
+      } catch (Exception $e) {
+        echo '<p>ただいま障害によりご迷惑をおかけしております。</p>';
+        exit();
+      }
+      ?>
 
-  商品情報参照
-  <br>
-  <br>
-  商品コード
-  <br>
-  <?php print $pro_code; ?>
-  <br>
-  <br>
-  商品名
-  <br>
-  <?php print $pro_name; ?>
-  <br>
-  <br>
-  価格
-  <br>
-  <?php print $pro_price; ?>
-  円
-  <br>
-  <br>
-  <?php print $disp_gazou; ?>
-  <br>
-  <form>
-    <input type="button" onclick="history.back()" value="戻る">
-  </form>
+      <p>商品情報参照</p>
+      <table border="1" class="cart-table">
+        <tr>
+          <th>商品コード</th>
+          <td><?php echo htmlspecialchars($pro_code, ENT_QUOTES, 'UTF-8'); ?></td>
+        </tr>
+        <tr>
+          <th>商品名</th>
+          <td><?php echo $pro_name; ?></td>
+        </tr>
+        <tr>
+          <th>価格</th>
+          <td><?php echo $pro_price; ?>円</td>
+        </tr>
+        <tr>
+          <th>画像</th>
+          <td><?php echo $disp_gazou; ?></td>
+        </tr>
+      </table>
+
+      <br>
+      <a href="shop_cartin.php?procode=<?php echo urlencode($pro_code); ?>" class="main__btn">カートに入れる</a>
+      <br><br>
+      <a href="javascript:history.back()" class="sub__btn">戻る</a>
+    </div>
+  </main>
 </body>
 
 </html>
