45f3ba485bd309194e189c51131a3f72f17809e9
chisato410 <it242243@trident.ac.jp>
2025-06-23 15:45:02 +0900

    cssの追加

--- shop/shop/shop_cartin.php
+++ shop/shop/shop_cartin.php
@@ -1,68 +1,76 @@
 <?php
 session_start();
 session_regenerate_id(true);
-if (isset($_SESSION['member_login']) == false) {
-  $member_message = 'ようこそ、ゲスト様 ';
-  $member_message .= '<a href="member_login.html">会員ログイン</a>';
-  $member_message .= '<br><br>';
+
+if (!isset($_SESSION['member_login'])) {
+  $member_message = <<<HTML
+ようこそ、ゲスト様
+<a href="member_login.html" class="login__btn">会員ログイン</a><br><br>
+HTML;
 } else {
-  $member_message = 'ようこそ、';
-  $member_message .= $_SESSION['member_name'];
-  $member_message .= '様 ';
-  $member_message .= '<a href="member_logout.php">ログアウト</a>';
-  $member_message .= '<br><br>';
+  $name = htmlspecialchars($_SESSION['member_name'], ENT_QUOTES, 'UTF-8');
+  $member_message = <<<HTML
+ようこそ、{$name}様
+<a href="member_logout.php" class="login__btn">ログアウト</a><br><br>
+HTML;
 }
 ?>
 <!doctype html>
-<html>
+<html lang="ja">
 
 <head>
   <meta charset="UTF-8">
-  <title> ろくまる農園 商品をカートに入れる</title>
+  <title>商品カート追加 | ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
 </head>
 
 <body>
+  <?php include '../common/header.php'; ?>
 
-  <?php
+  <main class="main">
+    <div class="main__inner">
+      <?php echo $member_message; ?>
 
-  print $member_message;
+      <?php
+      try {
+        $pro_code = filter_input(INPUT_GET, 'procode', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
+        if (!$pro_code) {
+          echo '<p>不正なアクセスです。</p>';
+          echo '<a href="shop_list.php" class="link-back">商品一覧に戻る</a>';
+          exit();
+        }
 
-  try {
-    $pro_code = filter_input(INPUT_GET, 'procode', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if (isset($_SESSION['cart'])) {
+          $cart = $_SESSION['cart'];
+          $kazu = $_SESSION['kazu'];
 
-    if (isset($_SESSION['cart']) == true) {
-      // セッションから商品と個数を取得する
-      $cart = $_SESSION['cart'];
-      $kazu = $_SESSION['kazu'];
+          if (in_array($pro_code, $cart)) {
+            echo '<p>その商品はすでにカートに入っています。</p>';
+            echo '<a href="shop_list.php" class="link-back">商品一覧に戻る</a>';
+            exit();
+          }
+        } else {
+          $cart = [];
+          $kazu = [];
+        }
 
-      // カートに既に入っている場合
-      if (in_array($pro_code, $cart) == true) {
-        print 'その商品はすでにカートに入っています。';
-        print '<br><br>';
-        print '<a href="shop_list.php">商品一覧に戻る</a>';
+        $cart[] = $pro_code;
+        $kazu[] = 1;
+        $_SESSION['cart'] = $cart;
+        $_SESSION['kazu'] = $kazu;
+      } catch (Exception $e) {
+        echo '<p>ただいま障害によりご迷惑をおかけしております。</p>';
         exit();
       }
-    }
+      ?>
 
-    // 商品を追加する
-    $cart[] = $pro_code;
-    $_SESSION['cart'] = $cart;
-
-    // 個数を追加する
-    $kazu[] = 1;
-    $_SESSION['kazu'] = $kazu;
-  } catch (Exception $e) {
-    print 'ただいま障害により大変ご迷惑をおかけしております。';
-    print $e->getMessage(); // エラーメッセージを出す
-    exit();
-  }
-  ?>
-
-  カートに追加しました。
-  <br>
-  <br>
-  <a href="shop_list.php">商品一覧に戻る</a>
+      <p>カートに追加しました。</p>
+      <a href="shop_list.php" class="main__btn">商品一覧に戻る</a>
+      <br><br>
+      <a href="shop_cartlook.php" class="sub__btn">カートを見る</a>
+    </div>
+  </main>
 </body>
 
 </html>
