1d3d38fa2182ff2b1711754e3a4ed01d51d808e0
chisato410 <it242243@trident.ac.jp>
2025-06-23 15:29:54 +0900

    数量変更画面にcssを追加

--- shop/shop/kazu_change.php
+++ shop/shop/kazu_change.php
@@ -1,42 +1,58 @@
 <?php
 session_start();
 session_regenerate_id(true);
+?>
+<!doctype html>
+<html lang="ja">
 
+<head>
+  <meta charset="UTF-8">
+  <title>数量変更エラー | ろくまる農園</title>
+  <?php include '../common/head.php'; ?>
+</head>
 
-$max = filter_input(INPUT_POST, 'max', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+<body>
+  <?php include '../common/header.php'; ?>
 
-for ($i = 0; $i < $max; $i++) {
-  $kazu[] = filter_input(INPUT_POST, 'kazu' . $i, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  <main class="main">
+    <div class="main__inner">
+      <?php
+      $max = filter_input(INPUT_POST, 'max', FILTER_VALIDATE_INT);
+      if ($max === false || $max < 0) {
+        echo '<p class="error-message">不正なアクセスです。</p>';
+        echo '<a href="shop_cartlook.php" class="link-back">カートに戻る</a>';
+        exit();
+      }
+      $kazu = [];
+      for ($i = 0; $i < $max; $i++) {
+        $input_kazu = filter_input(INPUT_POST, 'kazu' . $i, FILTER_SANITIZE_NUMBER_INT);
+        if (!preg_match('/\A[0-9]+\z/', $input_kazu)) {
+          echo '<p class="error-message">数量に誤りがあります。</p>';
+          echo '<a href="shop_cartlook.php" class="link-back">カートに戻る</a>';
+          exit();
+        }
+        if ($input_kazu < 1 || $input_kazu > 10) {
+          echo '<p class="error-message">数量は必ず1～10個までです。</p>';
+          echo '<a href="shop_cartlook.php" class="link-back">カートに戻る</a>';
+          exit();
+        }
+        $kazu[] = $input_kazu;
+      }
+      $cart = $_SESSION['cart'];
+      for ($i = $max - 1; $i >= 0; $i--) {
+        $sakujo = filter_input(INPUT_POST, 'sakujo' . $i, FILTER_DEFAULT);
+        if ($sakujo !== null) {
+          array_splice($cart, $i, 1);
+          array_splice($kazu, $i, 1);
+        }
+      }
+      $_SESSION['cart'] = $cart;
+      $_SESSION['kazu'] = $kazu;
+      header('Location: shop_cartlook.php');
+      exit();
+      ?>
+    </div>
+  </main>
+</body>
 
-  // 入力値が数値のみかチェック
-  if (preg_match('/\A[0-9]+\z/', $kazu[$i]) == 0) {
-    print '数量に誤りがあります。<br>';
-    print '<a href="shop_cartlook.php">カートに戻る</a>';
-    exit();
-  }
-
-  // 1～10のみ許可
-  if ($kazu[$i] < 1 || $kazu[$i] > 10) {
-    print '数量は必ず1～10個までです。<br>';
-    print '<a href="shop_cartlook.php">カートに戻る</a>';
-    exit();
-  }
-}
-
-$cart = $_SESSION['cart'];
-
-for ($i = $max - 1; $i >= 0; $i--) {
-  // 削除ボックスをチェックしている場合は取り除く
-  $sakujo = filter_input(INPUT_POST, 'sakujo' . $i, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-  if (isset($sakujo) == true) {
-    array_splice($cart, $i, 1);
-    array_splice($kazu, $i, 1);
-  }
-}
-
-$_SESSION['cart'] = $cart;
-
-$_SESSION['kazu'] = $kazu;
-
-header('Location: shop_cartlook.php');
-exit();
+</html>
